9ab4cfca4d990ecfcb726d72a88f6c1399c38a90
Alexandru031 <alexandrustanica33@gmail.com>
2023-09-21 09:35:31 +0200

    ejer_11 terminado

--- 2DAW/DEC/Ejercicio11/date.js
+++ 2DAW/DEC/Ejercicio11/date.js
@@ -1,4 +1,16 @@
 
-let horas = Date.now() / 3600000;
+let data = new Date()
 
-document.write(horas)
+let hora = data.getHours()
+
+function saludos() {
+    if (hora >= 9 && hora <= 12) {
+        return `Buenos días`
+    } else if (hora >= 13 && hora <= 19) {
+        return `Buenas tardes`
+    } else {
+        return `Buenas noches`
+    }
+}
+
+document.write(saludos())
