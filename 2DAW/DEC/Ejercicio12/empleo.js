863c183b9dc1e5723de3ea405601220034ab90eb
Alexandru031 <alexandrustanica33@gmail.com>
2023-09-21 13:39:51 +0200

    ejer_12 (Sol1)

--- 2DAW/DEC/Ejercicio12/empleo.js
+++ 2DAW/DEC/Ejercicio12/empleo.js
@@ -5,13 +5,17 @@
             this.departamento = 'Informático';
         }
 
+
         //Director
         // Clase que hereda de Empleado
-        function Director() {
+        function Director(nombre) {
             Empleado.call(this, nombre);
             this.informes = [];
             this.getNombreDir = function () {
                 return this.nombre
+            }
+            this.getInforme = function () {
+                return this.informes
             }
         }
 
@@ -19,7 +23,7 @@
         Director.prototype = new Empleado();
 
         // Estable el constructor de Director
-        Director.prototype.constructor = Director();
+        Director.prototype.constructor = new Empleado();
 
         function Trabajador(nombre) {
             Empleado.call(this, nombre);
@@ -45,24 +49,38 @@
         let dr1 = new Director("Pepito");
         let dr2 = new Director("Manolo");
 
-        Director = dr2.informes.push("Informe producción enero");
-        Director = dr2.informes.push("Informe producción febrero");
-        Director = dr2.informes.push("Informe producción marzo");
+        dr2.informes.push("Informe producción enero");
+        dr2.informes.push("Informe producción febrero");
+        dr2.informes.push("Informe producción marzo");
 
         let dr3 = new Director("Hortencia");
 
-        Director = dr3.informes.push("informe despido becario");
+        dr3.informes.push("informe despido becario");
 
         let ing1 = new Ingeniero("Juan");
         let ing2 = new Ingeniero("Carmen");
         let ing3 = new Ingeniero("Pablo");
         let ing4 = new Ingeniero("Emilio");
 
-        Ingeniero = ing1.LenguajeProgramacion('Java');
-        Ingeniero = ing2.LenguajeProgramacion('C++');
-        Ingeniero = ing3.LenguajeProgramacion('JS');
-        Ingeniero = ing4.LenguajeProgramacion('HTML5');
+        ing1.LenguajeProgramacion = "Java";
+        ing2.LenguajeProgramacion = "C++";
+        ing3.LenguajeProgramacion = "JS";
+        ing4.LenguajeProgramacion = "HTML";
 
-        document.write("________******** NOMBRES DIRECTORES ******** ________\n");
-        document.write("Director --> " + dr1.getNombreDir() + "\n")
-        document.write("Director --> " + dr2.getNombreDir() + "\n")
+        document.write("________******** NOMBRES DIRECTORES ******** ________" + `<br>`)
+        document.write("Director --> " + dr1.getNombreDir() + `<br>`)
+        document.write("Director --> " + dr2.getNombreDir() + `<br>`)
+        document.write("________******** INFORMES de: " + dr2.getNombreDir() + ` : <br>`)
+        for (let key in dr2.informes) {
+            document.write('------------------********* ' + dr2.informes[key] + `<br>`)
+        }
+        document.write(`Director --> ` + dr3.getNombreDir() + `<br>`)
+        document.write("________******** INFORMES de: " + dr3.getNombreDir() + ` : <br>`)
+        for (let key in dr3.informes) {
+            document.write('------------------********* ' + dr3.informes[key] + `<br><br>`)
+        }
+        document.write("________******** INGENIEROS ********________" + `<br>`)
+        document.write('------------------********* ' + ing1.getInforme() + `<br>`)
+        document.write('------------------********* ' + ing2.getInforme() + `<br>`)
+        document.write('------------------********* ' + ing3.getInforme() + `<br>`)
+        document.write('------------------********* ' + ing4.getInforme() + `<br>`)
