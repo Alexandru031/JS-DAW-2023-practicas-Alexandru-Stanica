2f68b0a4b421cbeefe6446b685641d6a9eda51c1
Alexandru031 <alexandrustanica33@gmail.com>
2023-09-26 10:37:10 +0200

    process ejer17

---
+++ 2DAW/DEC/Ejercicio17/getset.js
@@ -0,0 +1,31 @@
+/*
+Crea una clase llamada AlumnoDaw (nombre, apellido, email y edad) y creat sus getters y setters. Realiza todas las pruebas que consideres oportunas.
+*/
+
+class AlumnoDAW {
+    constructor(nombre, apellido, email, edad){
+        this.nombre = nombre
+        this.apellido = apellido
+        this.email = email
+        this.edad = edad
+    }
+
+    getDatos(){
+        return  `Nombre: ${this.nombre}\n` + 
+                `Apellido: ${this.apellido}\n` +
+                `Email: ${this.email}\n` + 
+                `Edad: ${this.edad}\n`
+    }
+
+    setNombre(nombre) {
+        return this.nombre = nombre
+    }
+
+    setEdad(edad) {
+        return this.edad = edad
+    }
+}
+
+let Usuario1 = new AlumnoDAW("Alexandru", "Stanica" , "hola@gmail.com", 20)
+
+console.log(Usuario1.getDatos())
