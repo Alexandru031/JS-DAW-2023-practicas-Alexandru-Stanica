3dfcdb9e3b14e2f2003804f2e0aa489a1ef00969
Alexandru031 <alexnadrustanica33@gmail.com>
2023-10-13 12:52:17 +0200

    actuatualizo ejer_23

--- 2DAW/DEC/Ejercicio23/eventos.js
+++ 2DAW/DEC/Ejercicio23/eventos.js
@@ -1,8 +1,9 @@
 document.addEventListener(`keydown`, function (event) {
     if (event.altKey && event.key === `F12`) {
-        const randomWidth = Math.floor(Math.random() * 1000); // Ancho aleatorio
-        const randomHeight = Math.floor(Math.random() * 1000); // Alto aleatorio
-        const imageUrl = `https://picsum.photos/${randomWidth}/${randomHeight}`;
+        const randomWidth = Math.floor(Math.random() * (4000 - 2000 + 1) + 2000); // Ancho aleatorio
+        /* const randomHeight = Math.floor(Math.random() * 2000); // Alto aleatorio */
+        /* const imageUrl = `https://picsum.photos/${randomWidth}/${randomHeight}`; */
+        const imageUrl = `https://picsum.photos/${randomWidth}`;
         document.getElementById('imagen').style.backgroundImage = `url("${imageUrl}")`;
     }
 })
