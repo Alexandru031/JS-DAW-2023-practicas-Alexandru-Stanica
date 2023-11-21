9167d879c7e7159a9ac0694b34f15b2b5ad0ff33
Alexandru031 <alexnadrustanica33@gmail.com>
2023-11-21 06:35:21 +0100

    Añade Temarios

--- 2DAW/DEC/UD1_javascript/Ejercicio9/find.js
+++ 2DAW/DEC/UD1_javascript/Ejercicio9/find.js
@@ -2,4 +2,4 @@ const array1 = [23, 30, 8, 55, 79]
 const array2 = [23, 30, 8, 0, 10];
 
 console.log(array1.find(el => el > 50))
-console.log(array.find(el => el > 50))
+console.log(array2.find(el => el > 50))
