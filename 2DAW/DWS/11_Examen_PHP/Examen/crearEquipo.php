bd13d661964d456cd0c90b0ba9c5912e91b67792
Alexandru031 <alexnadrustanica33@gmail.com>
2024-01-25 01:15:47 +0100

    DWS

--- 2DAW/DWS/11_Examen_PHP/Examen/crearEquipo.php
+++ 2DAW/DWS/11_Examen_PHP/Examen/crearEquipo.php
@@ -18,14 +18,21 @@ include "conexion.php";
 $conexion = conectar();
 
 try {
-    $db = $conexion->prepare("UPDATE equipo SET jugador1 = :jugador1 , jugador2 = :jugador2, jugador3 = :jugador3, jugador4 = :jugador4, jugador5 = :jugador5 WHERE usuario = :usuario");
+    $db = $conexion->prepare("UPDATE equipo SET jugador1 = :jugador1 , jugador2 = :jugador2, jugador3 = :jugador3, jugador4 = :jugador4, jugador5 = :jugador5, puntos = :puntos WHERE usuario = :usuario");
 
-    $db->bindValue(":jugador1", getIDJugador($conexion, $_GET['defensa']), PDO::PARAM_INT);
-    $db->bindValue(":jugador2", getIDJugador($conexion, $_GET['portero']), PDO::PARAM_INT);
-    $db->bindValue(":jugador3", getIDJugador($conexion, $_GET['centrocampista']), PDO::PARAM_INT);
-    $db->bindValue(":jugador4", getIDJugador($conexion, $_GET['delantero']), PDO::PARAM_INT);
-    $db->bindValue(":jugador5", getIDJugador($conexion, $_GET['extremo']), PDO::PARAM_INT);
+    $id_defensa = getIDJugador($conexion, $defensa);
+    $id_portero = getIDJugador($conexion, $portero);
+    $id_delantero = getIDJugador($conexion, $delantero);
+    $id_centrocampista = getIDJugador($conexion, $centrocampista);
+    $id_extremo = getIDJugador($conexion, $extremo);
+
+    $db->bindValue(":jugador1", $id_defensa, PDO::PARAM_INT);
+    $db->bindValue(":jugador2", $id_portero, PDO::PARAM_INT);
+    $db->bindValue(":jugador3", $id_delantero, PDO::PARAM_INT);
+    $db->bindValue(":jugador4", $id_centrocampista, PDO::PARAM_INT);
+    $db->bindValue(":jugador5", $id_extremo, PDO::PARAM_INT);
     $db->bindValue(":usuario", $_SESSION['nombre']);
+    $db->bindValue(":puntos", getPuntos(getPuntoJugador($conexion, $id_defensa, 1), getPuntoJugador($conexion, $id_portero, 1), getPuntoJugador($conexion, $id_delantero, 1), getPuntoJugador($conexion, $id_centrocampista, 1), getPuntoJugador($conexion, $id_extremo, 1)), PDO::PARAM_INT);
     $db->execute();
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
@@ -50,4 +57,8 @@ function getPuntoJugador($conect, $id, $jornada) {
         $punto = $row['puntos'];
     }
     return $punto;
+}
+
+function getPuntos($punto1, $punto2, $punto3, $punto4, $punto5){
+    return $punto1 + $punto2 + $punto3 + $punto4 + $punto5;
 }
